6154aaa2e617ea2effcca6e33d95921f04ad5d72
dz24 <daniel.t.zhang@ntnu.no>
2024-10-09 09:49:01 +0900

    update

--- dztools/mem/hel.py
+++ dztools/mem/hel.py
@@ -4,7 +4,7 @@ import typer
 
 
 def helicity(
-    gro: Annotated[str, typer.Option("-gro", help="gro file")],
+    top: Annotated[str, typer.Option("-top", help="gro/pdb/tpr file")],
     xtc: Annotated[str, typer.Option("-xtc", help="xtc file")] = None,
     lip: Annotated[str, typer.Option("-lip", help="xtc file")] = "POPC",
 ):
@@ -17,32 +17,23 @@ def helicity(
     import MDAnalysis as mda
     from MDAnalysis.analysis.dssp import DSSP
 
-    u = mda.Universe(gro, xtc)
+    u = mda.Universe(top, xtc)
 
     protein = u.select_atoms("protein")
-    nores = 26
+    nores = 25
     noprot = int(len(protein.residues)/nores)
-    # print(len(protein.residues))
-
-    s = DSSP(u).run().results.dssp
-    print(len(s))
-    print(s[0])
-    print(s[-1])
-
-    exit('a')
-
-
-
     mels = []
+    print('len', len(protein.residues))
     for i in range(noprot):
-        mels.append(protein.residues[nores*i:nores*(i+1)])
+        # mels.append(u.select_atoms(f"resid {nores*i}:{nores*(i+1)}"))
+        mel = u.select_atoms(f"resid {nores*i}:{nores*(i+1)}")
+        helicity = []
+        x = []
+        for x0, f in enumerate(DSSP(mel).run().results.dssp):
+            heli = sum(np.array(f)=='H')/nores
+            if heli > 0:
+                helicity.append(sum(np.array(f)=='H')/nores)
+                x.append(x0)
+        plt.scatter(x, helicity)
 
-    for idx, ts in enumerate(u.trajectory):
-
-        # DSSP(u).run().results.dssp[0]
-        # s = DSSP(u).run().results.dssp[0]
-        s = DSSP(mels[0]).run().results.dssp[0]
-
-    # print('tiger a', sum(resi), len(resi))
-        print('tiger b', sum(np.array(s)=='H'), len(s))
-    return sum(resi)/(len(resi)-2)
+    plt.show()
